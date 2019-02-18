532aaddeafaf1002bf681fe44107e67d196edde1
Krishna Pradhan <kpaccess@yahoo.com>
2019-02-18 11:38:19 -0500

    added new blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
 			resolve: `gatsby-source-filesystem`,
 			options: {
 				name: `markdown-pages`,
-				path: `${__dirname}/src/blogs`,
+				path: `${__dirname}/blog`,
 			},
 		},
 		`gatsby-transformer-remark`,
